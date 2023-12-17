1a2f6452f3c780213363c96a03aae8467489fda4
shiritani <shiritan1.chch@gmail.com>
2023-12-17 16:25:35 +0700

    final

--- Particles.cs
+++ Particles.cs
@@ -20,70 +20,35 @@ namespace StorybrewScripts
         {
             BeatDuration = GetBeatDuration(0);
 
-            GenerateParticles(53400, 74733, BeatDuration * 16, 72);
-            GenerateParticles(76067, 108067, BeatDuration * 16, 72);
-            GenerateParticles(108067, 129400, BeatDuration * 8, 32);
-            GenerateParticles(140400, 176733, BeatDuration * 6, 36);
-            GenerateParticles(179067, 221733, BeatDuration * 18, 80);
-            GenerateParticles(221733, 264400, BeatDuration * 16, 72);
-            GenerateParticles(297066, 318565, BeatDuration * 16, 72);
-            GenerateParticles(321232, 363898, BeatDuration * 8, 32);
-            GenerateParticles(363898, 402898, BeatDuration * 16, 24);
-
-            GenerateRaysParticles(53400, 73400, 100, 32);
+            GenerateParticles(500, 2522, BeatDuration * 20, 24);
+            GenerateParticles(85159, 93472, BeatDuration * 25, 16);
+            GenerateParticles(190347, 212178, BeatDuration * 35, 12);
+            GenerateParticles(290726, 297700, BeatDuration * 25, 16);
+            GenerateParticles(380276, 391112, BeatDuration * 22, 22);
         }
 
-        private void GenerateParticles(double startTime, double endTime, double particleDuration, int particleCount, string filePath = "sb/particle.png", float scale = 0.02f, double opacity = 1)
+        private void GenerateParticles(double startTime, double endTime, double particleDuration, int particleCount, string filePath = "sb/circle.png", float scale = 0.02f, double opacity = 1)
         {
             var bitmap = GetMapsetBitmap(filePath);
             var bitmapScale = bitmap.Height * scale;
             using (var pool = new OsbSpritePool(GetLayer("Particles"), filePath, OsbOrigin.Centre, (sprite, starttime, endtime) =>
             {
-                sprite.Fade(startTime - 1, startTime, 0, opacity);
-                sprite.Fade(endTime, 0);
+                sprite.Fade(startTime - 500, startTime, 0, 0.6);
+                sprite.Fade(endTime - 500, endTime, 0.6, 0);
             }))
             {
                 var timestep = particleDuration / particleCount;
                 for (double starttime = startTime - (particleDuration + BeatDuration * 12); starttime < endTime; starttime += timestep)
                 {
-                    var moveSpeed = Random(240, 360);
+                    var moveSpeed = Random(100, 150);
                     var endtime = starttime + Math.Ceiling(480f / moveSpeed) * particleDuration;
                     var sprite = pool.Get(starttime, endtime);
 
-                    var startX = Random(-107, 747f);
+                    var startX = Random(-107, 746);
                     sprite.MoveX(starttime, endtime, startX, startX + Random(-50, 50f));
-                    sprite.MoveY(OsbEasing.InQuart, starttime, endtime, 480 + bitmapScale, -bitmapScale);
-                    sprite.Scale(starttime, Random(scale, scale * 2.5));
+                    sprite.MoveY(starttime, endtime, 480, 240);
+                    sprite.Scale(starttime, Random(scale, scale * 10));
                     sprite.Additive(starttime, endtime);
-                }
-            }
-        }
-
-        private void GenerateRaysParticles(double startTime, double endTime, double speed, int particleCount)
-        {
-            using (var pool = new OsbSpritePool(GetLayer("Particles"), "sb/particle.png", OsbOrigin.Centre, (sprite, starttime, endtime) => { }))
-            {
-                var starttime = startTime;
-                while (starttime <= endTime)
-                {
-                    var lifeTime = Random(BeatDuration * 6, BeatDuration * 18);
-                    var timeStep = lifeTime / particleCount;
-                    var endtime = starttime + lifeTime;
-
-                    var moveAngle = Random(-Math.PI, -Math.PI * 2);
-                    var moveDistance = (float)speed * (float)lifeTime * 0.001f;
-
-                    var startPosition = new Vector2(Random(-20, 125f), Random(-5, 0));
-                    var endPosition = startPosition + new Vector2((float)Math.Cos(moveAngle), (float)Math.Sin(moveAngle)) * moveDistance;
-
-                    var sprite = pool.Get(starttime, endtime);
-                    sprite.Scale(starttime, Random(0.01, 0.07));
-                    sprite.Fade(OsbEasing.InCubic, starttime, endtime, 1, 0);
-
-                    sprite.Additive(starttime, endtime);
-                    sprite.Move(starttime, endtime, startPosition, endPosition);
-
-                    starttime += timeStep;
                 }
             }
         }
--- README.md
+++ README.md
@@ -2,9 +2,15 @@ osu! storyboard for my upcoming beatmap.
 Not really optimized but it works.
 
 Todo list if I have time:
-- Flash effect
-- HitObject highlighting
-- Extra scenes/SDs
-- Add some particles
+- Flash effect (nope too lazy)
+- HitObject highlighting (non)
+- Extra scenes/SDs (no)
+- Add some particles (added)
 
-I like Yuzusoft games. Go play them. 
+Code taken from various other projects and modified to my own liking for the storyboard.
+
+I like Yuzusoft games. Go play them.
+
+![Nozomi](<Screenshot (30).png>)
+
+Nozomi best girl btw
